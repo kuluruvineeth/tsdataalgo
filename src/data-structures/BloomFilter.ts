056557f4bc2fe27fd773cd26581a9aa53f140468
kuluruvineeth <kuluruvineeth8623@gmail.com>
2023-09-14 08:04:06 +0530

    - added disjointset, heap, counter and filter

---
+++ src/data-structures/BloomFilter.ts
@@ -0,0 +1,71 @@
+/**
+ * This is the default hash functions we would like to provide.
+ * It may not be all-encompassing, but its better than none.
+ * All numbers used are prime numbers.
+ */
+
+export type HashFunctions<T> = ReadonlyArray<(x: T) => number>;
+
+class BloomFilter<T> {
+  private _bits: Array<boolean>;
+
+  private _size: number;
+
+  private _hashFunctions: HashFunctions<T>;
+
+  static numberHashFunctions: HashFunctions<number> = [
+    (x: number): number => (25 * x + 13) % 31,
+    (x: number): number => (109 * x + 71) % 139,
+    (x: number): number => (677 * x + 241) % 859,
+    (x: number): number => (547 * x + 383) % 997,
+    (x: number): number => (173 * x + 149) % 499,
+  ];
+
+  /**
+   * Initialize a bloom filter with an appropriate size
+   * and array of hash functions.
+   *
+   * @param {number} size The size of the bloom filter to initialize as.
+   * @param {Function[]} hashFunctions The array of hash functions to use.
+   * @returns {undefined}
+   */
+  constructor(size: number, hashFunctions: HashFunctions<T>) {
+    this._bits = Array(size).fill(false);
+    this._size = size;
+    this._hashFunctions = hashFunctions;
+  }
+
+  /**
+   * Add an item to the bloom filter to register its membership.
+   * @param {*} item The item to be added.
+   */
+  add(item: T): void {
+    this._hashFunctions.forEach(hashFunction => {
+      // the mod _size is needed to ensure we do not go out of bounds
+      this._bits[hashFunction(item) % this._size] = true;
+    });
+  }
+
+  /**
+   * Test if an item is *probably* in the bloom filter.
+   * @param {*} item The item whose membership is to be tested.
+   * @returns {boolean} Whether the item is probably in the set or not.
+   *    If a false is returned, the item is surely not in the set.
+   */
+  contains(item: T): boolean {
+    // check if every bit at the location indicated by the hash funnction returns true
+    return this._hashFunctions.every(
+      hashFunction => this._bits[hashFunction(item) % this._size]
+    );
+  }
+
+  /**
+   * Returns the size of the bloom filter.
+   * @returns {number} Size of bloom filter.
+   */
+  get size(): number {
+    return this._size;
+  }
+}
+
+export default BloomFilter;
---
+++ src/data-structures/disjoint-set/DisjointSetV2.ts
@@ -0,0 +1,93 @@
+class DisjointSetV2 {
+  private _parents: Array<number>;
+
+  private _ranks: Array<number>;
+
+  private _setSizes: Array<number>;
+
+  private _size: number;
+
+  constructor(size: number) {
+    this._parents = Array(size)
+      .fill(null)
+      .map((_, index) => index);
+    this._ranks = Array(size).fill(0);
+    this._setSizes = Array(size).fill(1);
+    this._size = size;
+  }
+
+  /**
+   * The total number of elements in the elements set.
+   * @returns The total number of elements in the elements set.
+   */
+  sizeOf(element: number): number {
+    return this._setSizes[this.find(element)];
+  }
+
+  /**
+   * The number of disjoint sets.
+   * @returns {number} The number of disjoint sets.
+   */
+  get size(): number {
+    return this._size;
+  }
+
+  /**
+   * Find the set that an element belongs to.
+   * @param {number} element The element we are interested in.
+   * @returns {number} The root of the set that the element belongs to.
+   */
+  find(element: number): number {
+    if (this._parents[element] !== element) {
+      //path compression heuristic.
+      this._parents[element] = this.find(this._parents[element]);
+    }
+
+    return this._parents[element];
+  }
+
+  /**
+   * Determines if two elements are from the same set.
+   * @param {number} elementA The first element.
+   * @param {number} elementB The second element.
+   * @returns {boolean} Whether the two elements belongs to the same set.
+   */
+  isSameSet(elementA: number, elementB: number): boolean {
+    return this.find(elementA) === this.find(elementB);
+  }
+
+  /**
+   * Make elementA and elementB belong to the same set. Uses Union by rank.
+   * @param {number} elementA The first element.
+   * @param {number} elementB The second element.
+   */
+  union(elementA: number, elementB: number): void {
+    const rootA = this.find(elementA);
+    const rootB = this.find(elementB);
+
+    if (rootA === rootB) {
+      // Nothing to be done since elements already belong to the same set.
+      return;
+    }
+
+    // Reduce number of sets.
+    --this._size;
+
+    //Union by rank.
+    // Attaches the shorter tree to the root of the taller tree.
+    if (this._ranks[rootA] > this._ranks[rootB]) {
+      this._setSizes[rootA] += this._setSizes[rootB];
+      this._parents[rootB] = rootA;
+      return;
+    }
+
+    this._setSizes[rootB] += this._setSizes[rootA];
+    this._parents[rootA] = rootB;
+
+    if (this._ranks[rootA] === this._ranks[rootB]) {
+      ++this._ranks[rootB];
+    }
+  }
+}
+
+export default DisjointSetV2;
