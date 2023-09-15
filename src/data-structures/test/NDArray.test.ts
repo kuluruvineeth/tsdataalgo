f61293280539d620802dcf4e9e6e5669cc618f42
kuluruvineeth <kuluruvineeth8623@gmail.com>
2023-09-15 08:13:51 +0530

    - added tests for utils

---
+++ src/data-structures/test/NDArray.test.ts
@@ -0,0 +1,72 @@
+import NDArray from '../NDArray';
+
+describe('NDArray', () => {
+  test('one dimension', () => {
+    const arr = NDArray.create([7]);
+    expect(arr).toEqual([0, 0, 0, 0, 0, 0, 0]);
+    arr[3] = 1;
+    expect(arr).toEqual([0, 0, 0, 1, 0, 0, 0]);
+  });
+
+  test('two dimensions', () => {
+    const arr = NDArray.create([2, 7]);
+    expect(arr).toEqual([
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+    ]);
+    arr[1][3] = 1;
+    expect(arr).toEqual([
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0],
+    ]);
+  });
+
+  test('multi dimensions', () => {
+    const arr = NDArray.create([2, 3, 4]);
+    expect(arr).toEqual([
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+    ]);
+    arr[1][1][3] = 1;
+    expect(arr).toEqual([
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 0, 0],
+      ],
+    ]);
+  });
+
+  test('default value', () => {
+    expect(NDArray.create([7], 1)).toEqual([1, 1, 1, 1, 1, 1, 1]);
+    expect(NDArray.create([2, 7], 1)).toEqual([
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+    ]);
+    expect(NDArray.create([2, 3, 4], 1)).toEqual([
+      [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+      ],
+      [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+      ],
+    ]);
+  });
+});
---
+++ src/data-structures/test/Node.test.ts
@@ -0,0 +1,32 @@
+import Node from '../Node';
+
+describe('Node', () => {
+  test('constructor()', () => {
+    const n = new Node(100);
+    expect(n).toBeTruthy();
+    expect(n.value).toBe(100);
+    expect(n.next).toBe(null);
+    expect(n.prev).toBe(null);
+  });
+
+  test('value', () => {
+    const n1 = new Node(100);
+    expect(n1.value).toBe(100);
+    const n2 = new Node('foo bar');
+    expect(n2.value).toBe('foo bar');
+  });
+
+  test('next', () => {
+    const n1 = new Node(100);
+    const n2 = new Node(200);
+    n1.next = n2;
+    expect((n1.next as Node<number>).value).toBe(n2.value);
+  });
+
+  test('prev', () => {
+    const n1 = new Node(100);
+    const n2 = new Node(200);
+    n2.prev = n1;
+    expect((n2.prev as Node<number>).value).toBe(n1.value);
+  });
+});
