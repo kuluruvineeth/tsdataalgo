06dfa17124b933a31f7b3d3d83d25afd9f064ec5
kuluruvineeth <kuluruvineeth8623@gmail.com>
2023-09-15 08:05:50 +0530

    - fixing linting and type error

--- src/utils/DeepClone.ts
+++ src/utils/DeepClone.ts
@@ -4,6 +4,7 @@ export default function deepClone<T>(val: T): T {
   if (isArray(val)) {
     // @ts-expect-error: Hard to type this.
     return (
+      // @ts-expect-error: any is ok here as clone doesn't know anything about the argument.
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       Array((val as Array<any>).length)
         .fill(null)
